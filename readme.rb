16e9324d559ac91208a1da0fa5593bdb9578bfc4
AkaKwak <christophe.alaterre@gmail.com>
2024-10-22 10:32:31 +0200

    readme

---
+++ readme.rb
@@ -0,0 +1,18 @@
+# Gossip Project
+
+This is a simple Sinatra application that allows users to create, view, edit, and update gossips. Gossips are stored in a CSV file, and each gossip has an author and content.
+
+## Features
+
+- Create new gossips
+- View a list of all gossips
+- Edit existing gossips
+- Gossips are saved in a CSV file
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yourusername/gossip-project.git
+   cd gossip-project
